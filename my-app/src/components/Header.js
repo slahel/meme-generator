0e3e1fc66aec0d2e210ab911f9cac73b7bc7a8e9
slahel <74063357+slahel@users.noreply.github.com>
2022-03-19 19:32:43 +0100

    css

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Header() {
-  return <div className="header">Meme Generator</div>;
+  return (
+    <div className="header">
+      <h1>Meme Generator</h1>
+    </div>
+  );
 }

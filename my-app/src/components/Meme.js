c7d43ed2436d0de7d043a23ef3ad2abdd2ca2d38
slahel <74063357+slahel@users.noreply.github.com>
2022-03-19 19:36:15 +0100

    form set-up

---
+++ my-app/src/components/Meme.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Meme() {
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+        />
+        <button className="form--button">Get a new meme image 🖼</button>
+      </div>
+    </main>
+  );
+}
